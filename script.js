e1a8aa1e74c50bcbcbf85b2b5d478697093da81a
Hemant Gaikwad <103812753+hemantgaikwad1@users.noreply.github.com>
2026-08-08 12:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,43 +20,7 @@ for(let key in obj){
 		num=num-val;
 	}
 
-	/*
-IV = 4 (instead of writing IIII)
 
-IX = 9 (instead of VIIII)
-
-XL = 40 (instead of XXXX)
-
-XC = 90
-
-CD = 400
-
-CM = 900
-	*/
-if(val===1000 && num>=900){
-	str+="CM";
-	num-=900;
-}
-	if(val===500 && num>=400){
-		str+="CD";
-		num-=400;
-	}
-	if(val===100 && num>=90){
-	str+="XC";
-	num-=90;
-}
-	if(val===50 && num>=40){
-		str+="XL";
-		num-=40;
-	}
-	if(val===10 && num>=9){
-	str+="IX";
-	num-=9;
-}
-	if(val===5 && num>=4){
-		str+="IV";
-		num-=4;
-	}
 	
 }
 	return str;
